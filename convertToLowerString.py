b573773e069fc6c8047845f69ff09859019da0dc
SulemanMughal <sulemanmughal155@gmail.com>
2021-09-07 10:24:33 +0500

    Convert string to upper case

--- convertToLowerString.py
+++ convertToLowerString.py
@@ -1,3 +1,3 @@
-s1 = input("Enter String to convet in lower case: ")
+s1 = input("Enter String to convert in lower case: ")
 print("Using python .lower() : " , s1.lower())
 print("Using python .casefold() : " , s1.casefold())
---
+++ convertToUpperString.py
@@ -0,0 +1,2 @@
+s1 = input("Enter String to convert in upper case: ")
+print("Using python .upper() : " , s1.upper())
