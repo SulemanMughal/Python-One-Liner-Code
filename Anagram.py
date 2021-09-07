57accefadc4519ac5773493f9cef592264e4de20
SulemanMughal <sulemanmughal155@gmail.com>
2021-09-07 23:22:15 +0500

    Copy Files

--- Anagram.py
+++ Anagram.py
@@ -1,3 +1,5 @@
+# Anagrame Program
+# An anagram of a string is another string that contains the same characters, only the order of characters can be different. 
 from collections import Counter
 
 s1 = input("Enter first string: ")
